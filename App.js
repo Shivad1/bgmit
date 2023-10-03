cd09b925a690cf0c7090000888f306c0e6c74d91
Shivad1 <136087210+Shivad1@users.noreply.github.com>
2023-10-03 11:21:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React from 'react-dom'
+import Navbar from './components/navbar';
+
+import './App.css';
+let name = "Shiva"
+function App() {
+
+  return (
+    <>
+    <Navbar title="Jaidev footwear company" aboutText="about jaidev"/>
+    </>
+  );
+}
+export default App;
